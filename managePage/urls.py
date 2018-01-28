e78647805c460578339decce9b7dff458b757815
AlekseyZaytcev <aleksey2142.zaytcev@gmail.com>
2018-01-28 23:51:01 +0200

    Add navbar to base template and logic for user log out.

--- managePage/urls.py
+++ managePage/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.conf.urls import url
+
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    url(r'^$', views.home, name='home'),
+    url(r'^logout/$', views.user_logout, name='logout'),
 ]
--- managePage/views.py
+++ managePage/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 def home(request):
     return render(request, 'managePage/home.html')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('home')
