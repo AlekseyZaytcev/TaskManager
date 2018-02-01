81208699297ac21ce2a583659b7fc46572e6bef3
AlekseyZaytcev <aleksey2142.zaytcev@gmail.com>
2018-02-01 20:48:41 +0200

    Change static files location.

--- TaskManager/settings.py
+++ TaskManager/settings.py
@@ -122,9 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static', 'static_dev'),
+    os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_prod')
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
