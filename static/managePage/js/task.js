e932f2390f103fa501929a7c9160e01b8eb346b7
AlekseyZaytcev <aleksey2142.zaytcev@gmail.com>
2018-02-05 22:26:12 +0200

    Add update task function.

--- static/managePage/js/task.js
+++ static/managePage/js/task.js
@@ -39,3 +39,35 @@ function deleteTask(obj) {
             console.log("complete");
         });
 };
+
+function updateTask(obj) {
+    var taskId = (obj.id).slice(0, -7);
+    var textFieldId = taskId + 'Text';
+    var updateFormId = taskId + 'updateForm';
+    $('#' + textFieldId).toggle(250);
+    $('#' + updateFormId).toggle(250);
+};
+
+function updateTaskSend(obj) {
+    var updateFormId = obj.id + 'Form';
+    var data = $('#' + updateFormId + ' input').serialize();
+    $.ajax({
+            url: '/updateTask/',
+            type: 'POST',
+            data: data,
+        })
+        .done(function() {
+            var name = $('#' + updateFormId + ' input[name="updatedTaskName"]').val();
+            $('#' + updateFormId + ' input[name="updatedTaskName"]').val('');
+            var textFieldId = '#' + obj.id.slice(0, -6) + 'Text';
+            $(textFieldId).html(name);
+            $('#' + updateFormId).toggle(250);
+            $(textFieldId).toggle(250);
+        })
+        .fail(function() {
+            console.log("error");
+        })
+        .always(function() {
+            console.log("complete");
+        });
+};
