f2b924dac0863d2b33b9f6a50c17e6136764b647
zlosh <zlosh@ololoPC>
2018-02-23 01:09:20 +0200

    add some tests.

--- managePage/tests/test_views.py
+++ managePage/tests/test_views.py
@@ -37,9 +37,13 @@ class ViewTestClass(TestCase):
         print("setUp: Run once for every test method to setup clean data.")
         # for each test authenticate user
         self.client.login(username='Ruby', password='123456')
-         
+        
+        
+    #------------------------------finish setup for tests----------------------------------------------------
+    
+    
     def test_home_5Projects_and_30Tasks_exist_in_response(self):
-        # Because home view load all tasks(expects 30) 
+        # Because home view load all tasks(expects 30) and filter this in view for current user's projects 
         resp = self.client.get(reverse('home'))
         self.assertTrue(len(resp.context['projects_list']) == 5)
         self.assertTrue(len(resp.context['tasks_list']) == 30)    
@@ -48,4 +52,79 @@ class ViewTestClass(TestCase):
         resp = self.client.get(reverse('getTask'), {'project_id': 2})
         self.assertTrue(len(resp.context['projects_list']) == 1)
         self.assertTrue(len(resp.context['tasks_list']) == 5)
+        
+    def test_create_project_if_empty_name_404Expected(self):
+        project_name = ''
+        resp = self.client.post(reverse('createProject'), {'projectName': project_name})
+        self.assertTrue(resp.status_code == 404)
+        
+    def test_create_project_if_name_exist_404Expected(self):
+        project_name = 'Project 2'
+        resp = self.client.post(reverse('createProject'), {'projectName': project_name})
+        self.assertTrue(resp.status_code == 404)
+        
+    def test_update_project_if_empty_name_404Expected(self):
+        project_name = ''
+        resp = self.client.post(reverse('updateProject'), {'updatedProjectName': project_name})
+        self.assertTrue(resp.status_code == 404)
+        
+    def test_update_project_if_name_exist_404Expected(self):
+        project_name = 'Project 2'
+        resp = self.client.post(reverse('updateProject'), {'updatedProjectName': project_name})
+        self.assertTrue(resp.status_code == 404)
+        
+    def test_create_task_if_empty_name_404Expected(self):
+        task_name = ''
+        resp = self.client.post(reverse('createTask'), {'task_name': task_name, 'project_id': 2})
+        self.assertTrue(resp.status_code == 404)
+        
+    def test_create_task_if_name_exist_404Expected(self):
+        task_name = 'Task 2'
+        resp = self.client.post(reverse('createTask'), {'task_name': task_name, 'project_id': 2})
+        self.assertTrue(resp.status_code == 404)
+        
+    def test_update_task_if_empty_name_404Expected(self):
+        taskName = ''
+        project_id = 2
+        task_id = 4
+        resp = self.client.post(reverse('updateTask'), {'updatedTaskName': taskName, 'task_id': task_id, 'project_id': project_id})
+        self.assertTrue(resp.status_code == 404)
+        
+    def test_update_task_if_name_exist_404Expected(self):
+        taskName = 'Task 2'
+        project_id = 2
+        task_id = 4
+        resp = self.client.post(reverse('updateTask'), {'updatedTaskName': taskName, 'task_id': task_id, 'project_id': project_id})
+        self.assertTrue(resp.status_code == 404)
+        
+    def test_check_task_expected_True_status(self):
+        #tasks creates with False status by default, checkView change status to True or if status True -> False
+        resp = self.client.post(reverse('checkTask'), {'task_id': 2})
+        self.assertTrue(Task.objects.filter(id=2)[0].status == True)
+        self.assertTrue(resp.status_code == 200)
+        
+    def test_shiftView_is_Task2and3_shifted(self):
+        # range(5) create this range < 0, 1, 2, 3, 4 >
+        # Take first project with tasks(id=3, id=4). Task 2 have status=True, id=3, name='Task 2'. After shift id=3 -> id=4, other is same
+        # Task 3 have: id=4, name='Task 3', status=False. shift -> id=3 and status and name are not changed
+
+        # Change status Task 2 to True and make shift
+        Task.objects.filter(id=3).update(status=True) 
+        resp = self.client.post(reverse('shiftTask'), {'project_id': 1, 'idStartdrag': 3, 'idFinishdrag': 4})
+        
+        # Check if Task 2 shifted(id=4 have status False -> True, name 'Task 3' -> 'Task 2':
+        self.assertTrue(Task.objects.filter(id=4)[0].status == True)
+        self.assertTrue(Task.objects.filter(id=4)[0].task_name == 'Task 2')
+        
+        # Check if Task 3 shifted(id=3 have status True -> False, name 'Task 2' -> 'Task 3':
+        self.assertTrue(Task.objects.filter(id=3)[0].status == False)
+        self.assertTrue(Task.objects.filter(id=3)[0].task_name == 'Task 3')
+        
+        # Response 200
+        self.assertTrue(resp.status_code == 200)
+    
+    def test_deadlineView_deadline_is_empty(self):
+        resp = self.client.post(reverse('setDeadline'), {'deadline': ''})
+        self.assertTrue(resp.status_code == 404)
+        
     
